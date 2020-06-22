88224586692ca7030ffa73cfb0b6f6b21526d927
Matthew Choat <matthewchoat@gmail.com>
2020-06-21 23:19:40 -0500

    Finished array sort and reverse challenges

---
+++ udemy_exercises/sort/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] array = { 14, 45, 53, 77, 111, 129, 215};
+
+        System.out.println("Array = " + Arrays.toString(array));
+
+        reverse(array);
+
+        System.out.println("Reversed array = " + Arrays.toString(array));
+    }
+
+    private static void reverse(int[] array) {
+
+        int maxIndex = array.length -1;
+        int halfLength = array.length / 2;
+        for(int i=0; i< halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex -i];
+            array[maxIndex - i] = temp;
+        }
+    }
+}
